45bbc3cb9fb2fa84b9d612cc32598b0058b5ebae
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-11 12:02:32 +0530

    Create basics2.js

---
+++ basics/basics2.js
@@ -0,0 +1,14 @@
+// let  password=prompt("enter your password");
+// let newpass= password.trim();
+// console.log(newpass);
+// console.log(password);
+
+let name="sristy";
+ let final=name.toUpperCase();
+console.log(final);
+let msg="sristy";
+ let index=msg.indexOf("r");
+console.log(index);
+let bca="    gjedjeh";
+let newbca=bca.toUpperCase().trim();
+console.log(newbca);
