f5fe58fed12d4d7d5d49ffc901dacae6f1acaf49
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-07-11 12:03:38 +0530

    Create object3.js

---
+++ basics/object3.js
@@ -0,0 +1,20 @@
+// destructuring in object
+
+const course={
+    coursename:"js in hindi",
+    instructor:"hitesh",
+    fees:"898"
+}
+// here we are destructuring it 
+const {instructor:teacher}=course// here we can instructor as teacher and we are extracting this value from course
+console.log(teacher)
+
+
+// json:-javasript object notation
+
+// json syntax
+// {
+//     "name":"sristy",
+//     "coursename":"js in hindi",
+//     "price":"898"
+// }
