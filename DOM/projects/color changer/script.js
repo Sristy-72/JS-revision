6b2a0280aba53d299f97abaa2f59c001b0bf16e4
Sristy <126855796+Sristy-72@users.noreply.github.com>
2024-10-12 17:05:20 +0530

    Create script.js

---
+++ DOM/projects/color changer/script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.button');
+// console.log(buttons)
+const body = document.querySelector('body');
+buttons.forEach(function (button) {
+  // console.log(button);
+  button.addEventListener('click', function (event) {
+    //  console.log(event)
+    //  console.log(event.target)
+    if (event.target.id === 'grey') {
+      body.style.backgroundColor = 'grey';
+    }
+    if (event.target.id === 'blue') {
+      body.style.backgroundColor = 'blue';
+    }
+    if (event.target.id === 'yellow') {
+      body.style.backgroundColor = 'yellow';
+    }
+    if (event.target.id === 'white') {
+      body.style.backgroundColor = 'white';
+    }
+  });
+});
